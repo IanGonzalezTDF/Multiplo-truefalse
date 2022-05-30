b6a187f8d81ac1551d0a1d85dc20844ed570f2bb
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-05-29 21:23:02 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let boton = document.getElementById("calcular");
+
+boton?.addEventListener("click", () => {
+  let a: number = Number(dato1.value);
+  let b: number = Number(dato2.value);
+
+  function esMultiplo() {
+    if (a % b == 0) {
+      return "true";
+    } else {
+      return "false";
+    }
+  }
+  console.log(esMultiplo());
+});
